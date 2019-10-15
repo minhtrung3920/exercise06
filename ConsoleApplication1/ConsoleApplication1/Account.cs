44bab03e9eb8a97cfbd00e6064b14103419b6bdd
quanganh <bantia447@gmail.com>
2019-10-15 15:28:08 +0700

    pending

---
+++ ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Account
+    {
+        int money = Convert.ToInt32(Console.ReadLine());
+        public Account(int amount)
+        {
+            money = amount;
+            Console.WriteLine("account");
+            Console.WriteLine("Deposit");
+            Console.WriteLine("withdrawal");
+            Console.WriteLine("Balance");
+        }
+        public void Withdrawals(int amount)
+        {
+            money -= amount;
+        }
+        public void Deposit(int amount)
+        {
+            money += amount;
+        }
+        public int Balance()
+        {
+            return 0;
+        }
+    }
+}
