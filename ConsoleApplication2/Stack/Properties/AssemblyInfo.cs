3dca8675cc20254e61ca821d7cec176094c257b3
minhtrung3920 <trung392dg@gmail.com>
2019-10-28 20:42:42 +0700

    stack

--- ConsoleApplication2/Stack/Properties/AssemblyInfo.cs
+++ ConsoleApplication2/Stack/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTest2")]
+[assembly: AssemblyTitle("Stack")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTest2")]
+[assembly: AssemblyProduct("Stack")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85d60d7f-2708-4908-856d-bf33c5dde59c")]
+[assembly: Guid("3975f44b-a978-492e-be82-c4d1982cdfc5")]
 
 // Version information for an assembly consists of the following four values:
 //
--- ConsoleApplication2/UnitTest2/UnitTest1.cs
+++ ConsoleApplication2/UnitTest2/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ConsoleApplication2;
+using STACKK;
 
 namespace UnitTest2
 {
